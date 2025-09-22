77c69fc6f2e01a656ce760f426203f8b217531b1
room-208 <yumalegomindstorm@gmail.com>
2025-09-22 22:15:47 +0900

    commit

--- bin/alb-accesstoken-cognito-authorizer.ts
+++ bin/alb-accesstoken-cognito-authorizer.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { AwsSolutionsChecks } from "cdk-nag";
+import { AwsSolutionsChecks, NagSuppressions } from "cdk-nag";
 import "dotenv/config";
 import { AlbAccesstokenCognitoAuthorizerStack } from "../lib/alb-accesstoken-cognito-authorizer-stack";
 
@@ -12,3 +12,18 @@ const stack = new AlbAccesstokenCognitoAuthorizerStack(
   app,
   "AlbAccesstokenCognitoAuthorizerStack"
 );
+
+NagSuppressions.addStackSuppressions(stack, [
+  {
+    id: "AwsSolutions-VPC7",
+    reason: "テスト用なので必要ない",
+  },
+  {
+    id: "AwsSolutions-IAM5",
+    reason: "テスト用なので必要ない",
+  },
+  {
+    id: "AwsSolutions-COG3",
+    reason: "テスト用なので必要ない",
+  },
+]);
--- lib/alb-accesstoken-cognito-authorizer-stack.ts
+++ lib/alb-accesstoken-cognito-authorizer-stack.ts
@@ -1,17 +1,21 @@
 import * as cdk from "aws-cdk-lib";
 import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import * as cognito from "aws-cdk-lib/aws-cognito";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as route53 from "aws-cdk-lib/aws-route53";
-import { NagSuppressions } from "cdk-nag";
 import { Construct } from "constructs";
 
 export class AlbAccesstokenCognitoAuthorizerStack extends cdk.Stack {
   private readonly domainName: string;
+  private readonly albDomainName: string;
+  private readonly userPoolDomainPrefix: string;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     this.domainName = process.env.DOMAIN_NAME || "";
+    this.albDomainName = this.domainName;
+    this.userPoolDomainPrefix = process.env.USER_POOL_DOMAIN_PREFIX || "";
 
     const vpc = new ec2.Vpc(this, "Vpc", {
       maxAzs: 2,
@@ -30,20 +34,52 @@ export class AlbAccesstokenCognitoAuthorizerStack extends cdk.Stack {
       ],
     });
 
-    NagSuppressions.addResourceSuppressions(vpc, [
-      {
-        id: "AwsSolutions-VPC7",
-        reason: "テスト用なので使用しない",
-      },
-    ]);
-
     const hostedZone = new route53.HostedZone(this, "HostedZone", {
       zoneName: this.domainName,
     });
 
-    const certificate = new acm.Certificate(this, "MyCertificate", {
+    const certificate = new acm.Certificate(this, "Certificate", {
       domainName: this.domainName,
       validation: acm.CertificateValidation.fromDns(hostedZone),
+    });
+
+    const userPool = new cognito.UserPool(this, "UserPool", {
+      selfSignUpEnabled: false,
+      signInAliases: {
+        email: true,
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+        requireSymbols: true,
+      },
+      mfa: cognito.Mfa.REQUIRED,
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+    });
+
+    const userPoolClient = new cognito.UserPoolClient(this, "UserPoolClient", {
+      userPool,
+      generateSecret: true,
+      authFlows: {
+        userPassword: true,
+        userSrp: true,
+      },
+      oAuth: {
+        flows: {
+          authorizationCodeGrant: true,
+        },
+        scopes: [cognito.OAuthScope.OPENID],
+        callbackUrls: [`https://${this.albDomainName}/oauth2/idpresponse`],
+      },
+    });
+
+    const userPoolDomain = new cognito.UserPoolDomain(this, "UserPoolDomain", {
+      userPool,
+      cognitoDomain: {
+        domainPrefix: this.userPoolDomainPrefix,
+      },
     });
   }
 }
